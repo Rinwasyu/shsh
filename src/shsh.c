3b6999201fc5a23d5c45b065d6ed1cb667dbacae
Rinwasyu <rinwakun@gmail.com>
2019-07-06 13:02:48 +0900

    support ↔ and fix bugs

--- src/shsh.c
+++ src/shsh.c
@@ -28,7 +28,7 @@
 
 #define PROG_NAME "shsh"
 #define PROG_FULLNAME "(((ง'ω')و三 ง'ω')ڡ≡ shsh"
-#define PROG_VERSION "0.0.x-alpha"
+#define PROG_VERSION "0.0.2-alpha"
 
 /*
 TODO: ↑ → ↓ ←
@@ -48,9 +48,9 @@ void print_help() {
 	printf("\t-h, --help\n");
 }
 
-void exec_command(char *command) {
-	// TODO: Search for PATH
-	// TODO: Support built-in commands (cd, export, etc.)
+void exec_command(char *command, char **envp) {
+	// TODO: Search for PATH...done
+	// TODO: Support built-in coumands (cd, export, etc.)
 	// TODO: Run ShellScript
 	char *prog = NULL;
 	char *args[BUF_SIZE] = {NULL};
@@ -62,7 +62,8 @@ void exec_command(char *command) {
 	args[0] = prog;
 	for (int i = 1; i < BUF_SIZE && (args[i] = strtok(NULL, " ")) != NULL; i++);
 
-	char *shsh_path = getenv("PATH");
+	char shsh_path[BUF_SIZE] = {0};
+	snprintf(shsh_path, BUF_SIZE, getenv("PATH"));
 	char *path = strtok(shsh_path, ":");
 	do {
 		char prog_path[BUF_SIZE] = {0};
@@ -70,12 +71,22 @@ void exec_command(char *command) {
 		snprintf(prog_path + strlen(prog_path), BUF_SIZE - strlen(prog_path), "/");
 		snprintf(prog_path + strlen(prog_path), BUF_SIZE - strlen(prog_path), prog);
 		// PATH/prog
-		execv(prog_path, args);
+		execve(prog_path, args, envp);
 	} while ((path = strtok(NULL, ":")) != NULL);
 	// ./prog
-	execv(prog, args);
+	execve(prog, args, envp);
 
 	printf("%s: %s: command not found\n", PROG_NAME, command);
+}
+
+void shsh_init() {
+	system("stty stop undef");
+	system("stty start undef");
+	system("stty -echo -icanon min 1 time 0");
+}
+
+void shsh_exit() {
+	system("stty sane");
 }
 
 int main(int argc, char **argv, char **envp) {
@@ -89,7 +100,7 @@ int main(int argc, char **argv, char **envp) {
 				int pid = fork();
 				if (pid == 0) {
 					if (argc > i+1) {
-						exec_command(argv[i+1]);
+						exec_command(argv[i+1], envp);
 					} else {
 						printf("%s: -c: option requires an argument\n", PROG_NAME);
 					}
@@ -123,21 +134,99 @@ int main(int argc, char **argv, char **envp) {
 	char c;
 	char *command = (char *)malloc(sizeof(char) * BUF_SIZE);
 	char **command_history = (char **)malloc(sizeof(char *) * BUF_SIZE);
+	int cursor_x = 0;
+	// Input mode
+	enum {Insert, Esc, Bracket, Numpad} mode = Insert;
 
-	printf("<%s@%s> $ ", getenv("LOGNAME"), strtok(getenv("HOSTNAME"), "."));
+	char shsh_logname[BUF_SIZE] = {0};
+	char shsh_hostname[BUF_SIZE] = {0};
+
+	snprintf(shsh_logname, BUF_SIZE, getenv("LOGNAME"));
+	snprintf(shsh_hostname, BUF_SIZE, getenv("HOSTNAME"));
+
+	shsh_init();
+	printf("<%s@%s> $ ", shsh_logname, strtok(shsh_hostname, "."));
 	while ((c = getchar()) != EOF) {
+		// DEBUG
+		//printf("keycode : %d\n", c);
 		if (c == 10) { // Enter
+			printf("\n");
 			int pid = fork();
 			if (pid == 0) {
-				exec_command(command);
+				shsh_exit();
+				exec_command(command, envp);
 				return -1;
 			}
 			waitpid(pid, NULL, 0);
+			shsh_init();
 			memset(command, 0, sizeof(char) * BUF_SIZE);
-			printf("<%s@%s> $ ", getenv("LOGNAME"), strtok(getenv("HOSTNAME"), "."));
+			printf("<%s@%s> $ ", shsh_logname, strtok(shsh_hostname, "."));
+			mode = Insert;
+			cursor_x = 0;
 		} else {
-			if (strlen(command) < BUF_SIZE) {
-				command[strlen(command)] = c;
+			// TODO: here
+			if (mode == Insert) {
+				if (c == 4) { // Ctrl-D
+					printf("exit");
+					shsh_exit();
+					break;
+				} else if (c == 27) {
+					mode = Esc;
+				} else if (c == 127) {
+					if (strlen(command) > 0) {
+						command[strlen(command)-1] = '\0';
+						printf("\b \b");
+						cursor_x--;
+					}
+				} else {
+					// input
+					if (strlen(command) < BUF_SIZE - 1) {
+						for (int i = cursor_x; i < strlen(command); i++)
+							printf(" ");
+						for (int i = cursor_x; i < strlen(command); i++)
+							printf("\b");
+
+						for (int i = cursor_x, ch = c; i < strlen(command) + 1; i++) {
+							char swap = command[i];
+							printf("%c", ch);
+							command[i] = ch;
+							ch = swap;
+						}
+
+						for (int i = 0; i < strlen(command) - cursor_x - 1; i++)
+							printf("\b");
+						cursor_x++;
+					}
+				}
+			} else if (mode == Esc) {
+				if (c == 79 || c == 91) {
+					mode = Bracket;
+				} else {
+					mode = Insert;
+				}
+			} else if (mode == Bracket) {
+				if (c == 49 || c == 52) {
+					mode = Numpad;
+				} else if (c == 65) {
+					mode = Insert;
+				} else if (c == 66) {
+					mode = Insert;
+				} else if (c == 67) { // RIGHT→
+					if (cursor_x < strlen(command)) {
+						printf("\e[1C");
+						cursor_x++;
+					}
+					mode = Insert;
+				} else if (c == 68) { // Left←
+					if (cursor_x > 0) {
+						printf("\e[1D");
+						cursor_x--;
+					}
+					mode = Insert;
+				}
+				// TODO:
+			} else if (mode == Numpad) {
+				mode = Insert;
 			}
 		}
 	}
